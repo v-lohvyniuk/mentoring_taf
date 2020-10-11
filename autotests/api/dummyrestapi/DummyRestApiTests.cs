3292cd853cf0ea05b88918ec45f81f94e1deff07
v-lohvyniuk <volodymyr.lohvyniuk@gmail.com>
2020-10-11 22:30:13 +0300

    Add RestSharp basic functionality

---
+++ autotests/api/dummyrestapi/DummyRestApiTests.cs
@@ -0,0 +1,27 @@
+﻿using mentoring_taf.taf.core.api.client;
+using NUnit.Allure.Core;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace mentoring_taf.autotests.api.dummyrestapi
+{
+    [AllureNUnit]
+    [TestFixture]
+    [Parallelizable(ParallelScope.All)]
+    class DummyRestApiTests
+    {
+        [Test]
+        public void GetDummyApiAllEmployees_PositiveTestCase()
+        {
+            RequestData requestData = new RequestData()
+                .WithUrl("/employee")
+                .WithQueryParam("all", "true");
+
+            ApiClient.GetInstance()
+                .WithRequestData(requestData)
+                .DoGet();
+        }
+    }
+}
---
+++ taf/core/api/client/ApiClient.cs
@@ -0,0 +1,53 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http.Headers;
+using System.Text;
+
+namespace mentoring_taf.taf.core.api.client
+{
+    public class ApiClient
+    {
+        private static readonly string DEFAULT_BASE_URL = "http://dummy.restapiexample.com/";
+
+        private static ApiClient apiClient;
+        
+        private string baseUrl;
+        private RestClient restClient;
+        private RequestData requestData;
+
+        private ApiClient(string baseUrl, RestClient client)
+        {
+            this.baseUrl = baseUrl;
+            this.restClient = client;
+            this.restClient.BaseUrl = new Uri(this.baseUrl);
+        }
+
+        public static ApiClient GetInstance()
+        {
+            if(null == apiClient)
+            {
+                RestClient client = new RestClient();
+                apiClient = new ApiClient(DEFAULT_BASE_URL, client);
+            }
+            return apiClient;
+        }
+
+        public ApiClient WithRequestData(RequestData requestData)
+        {
+            apiClient.requestData = requestData;
+            return this;
+        }
+
+        public void DoGet()
+        {
+            RestRequest request = new RestRequest(this.requestData.Url, Method.GET);
+            foreach(var item in this.requestData.QueryParams)
+            {
+                request.AddQueryParameter(item.Key, item.Value);
+            }
+            this.restClient.Execute(request);
+        }
+    }
+}
